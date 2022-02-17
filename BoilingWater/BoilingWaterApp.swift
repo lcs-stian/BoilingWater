876fa6a5e37723820250546b002b9425836efefb
Suzanne <STian@lcs.on.ca>
2022-02-17 08:36:13 -0500

    Initial Commit

---
+++ BoilingWater/BoilingWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoilingWaterApp.swift
+//  BoilingWater
+//
+//  Created by Suzanne Tian on 2022-02-17.
+//
+
+import SwiftUI
+
+@main
+struct BoilingWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
